da2816ea26932cb7c31bad6ea99e384dc87732e8
Alex <alexgarcia1771@outlook.com>
2020-12-17 19:25:25 +0100

    alex rama

--- routes/albaniles.js
+++ routes/albaniles.js
@@ -3,4 +3,12 @@ const router = express.Router();
 
 const Albaniles = require('../models/Albaniles');
 
+router.get('/albaniles', (req, res, next) => {
+    Albaniles.find()
+    .then(data => {
+        res.status(200).json(data)
+    })
+})
+
+
 module.exports = router;
