5d65b16a5b8babb10b75fe0d01a12b6b95fe535a
Alex <alexgarcia1771@outlook.com>
2020-12-18 16:16:17 +0100

    rama master a heroku

--- app.js
+++ app.js
@@ -68,6 +68,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 
+app.use((req, res)=> {
+  res.sendFile(_dirname + "/public/index.htm");
+});
+
 
 // ROUTER MIDDLEWARE
 app.use("/auth", auth);
@@ -77,10 +81,6 @@ app.use("/api", locksmith);
 app.use('/api', setup);
 app.use('/api', fileUp);
 
-
-app.use((req, res)=> {
-  res.sendFile(_dirname + "/public/index.htm");
-});
 
 
 // ERROR HANDLING
