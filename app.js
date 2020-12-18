eca6bd81ef8bdc4a837e2dbf71be06eb7c1c20cd
Alex <alexgarcia1771@outlook.com>
2020-12-18 10:33:01 +0100

    pruebaprueba

--- app.js
+++ app.js
@@ -67,6 +67,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res)=> {
+  res.sendFile(_dirname + "/public/index.htm");
+});
+
 
 // ROUTER MIDDLEWARE
 app.use("/auth", auth);
@@ -76,9 +80,7 @@ app.use("/api", locksmith);
 app.use('/api', setup);
 app.use('/api', fileUp);
 
-app.use((req, res)=> {
-  res.sendFile(_dirname + "/public/index.htm");
-});
+
 
 // ERROR HANDLING
 // catch 404 and forward to error handler
