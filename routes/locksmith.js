7c0d8a1e5af175b4bdb35ce3eda05c4a87eeef3c
Alex <alexgarcia1771@outlook.com>
2020-12-19 18:58:26 +0100

    rama-alex

--- routes/locksmith.js
+++ routes/locksmith.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const Tiendas = require('../models/Tiendas');
 
 
-router.get('/locksmith', (req, res, next) => {
+router.get('/locksmith', (req, res, next) => { 
     Tiendas.find()
     .then((data) => {
         res.json(data)
@@ -15,16 +15,23 @@ router.get('/locksmith', (req, res, next) => {
 })
 
 router.post('/locksmith', async (req, res, next) => {
-  Tiendas.create(req.body)
-  .then( newTienda => {
-      console.log('Created new tienda: ', newTienda);
-      res.json(newTienda)
-  })
-  .catch( err => {
-      console.log(err)
-      next(err)
-  })
-  
+    let newTiendas = new Tiendas(req.body);
+    if (!req.body.title) {
+      return res.status(400).send({
+        success: 'false',
+        message: 'title is required',
+      });
+    }
+    try {
+      const tienda = await newTiendas.save();
+      res.status(200).json(tienda);
+      // return saveImagetoUser(aNewImage)
+    } catch (error) {
+      res.status(400).send({
+        success: 'false',
+        message: 'Unable to save to database',
+      });
+    }
 })
 
 const saveImagetoUser = async (data) => {
