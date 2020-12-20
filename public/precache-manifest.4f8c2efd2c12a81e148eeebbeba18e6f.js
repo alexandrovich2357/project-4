c0a7ffb1892f7516108edf5a27a772fb678e927e
Alex <alexgarcia1771@outlook.com>
2020-12-20 17:53:19 +0100

    deploy final casi

--- public/precache-manifest.4f8c2efd2c12a81e148eeebbeba18e6f.js
+++ public/precache-manifest.4f8c2efd2c12a81e148eeebbeba18e6f.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f25e9fb4e0536a923ff3403f2fbbbe5b",
+    "revision": "8739bd0433a6cbfe5c12cb57cd29b23c",
     "url": "/index.html"
   },
   {
-    "revision": "ea82a1e118153e9e10f1",
+    "revision": "3ac969d22d666b520e8e",
     "url": "/static/css/main.4133c0ed.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.6e7010a3.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ea82a1e118153e9e10f1",
-    "url": "/static/js/main.24945141.chunk.js"
+    "revision": "3ac969d22d666b520e8e",
+    "url": "/static/js/main.b17a0590.chunk.js"
   },
   {
     "revision": "5179ff928fc7dafdbfe9",
