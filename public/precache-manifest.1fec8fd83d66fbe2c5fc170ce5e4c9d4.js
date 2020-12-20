34a1d42863e9e85d2e00acd7f3585acbf27416f0
Alex <alexgarcia1771@outlook.com>
2020-12-20 18:39:43 +0100

    alexander

--- public/precache-manifest.1fec8fd83d66fbe2c5fc170ce5e4c9d4.js
+++ public/precache-manifest.1fec8fd83d66fbe2c5fc170ce5e4c9d4.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "94c5ac88c4d19f7f3bd90da793aaa895",
+    "revision": "dbf5aabe7582d430096a3432785cd8c9",
     "url": "/index.html"
   },
   {
-    "revision": "7ae628270d5238ff729d",
+    "revision": "d091aba3b0a61f5dfcfb",
     "url": "/static/css/main.4133c0ed.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.6e7010a3.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "7ae628270d5238ff729d",
-    "url": "/static/js/main.3ad5ed80.chunk.js"
+    "revision": "d091aba3b0a61f5dfcfb",
+    "url": "/static/js/main.937fd0e7.chunk.js"
   },
   {
     "revision": "5179ff928fc7dafdbfe9",
